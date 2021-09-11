748c064105af2967f8e757d40613b46206445b49
Danny Mussie <dannymussie7@gmail.com>
2021-09-11 17:57:00 -0500

    edited tasks.router.js to account for change in my_tasks database

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -8,11 +8,11 @@ const pool = require('../modules/pool');
 
 // GET
 tasksRouter.get('/', (req, res) => {
-    const queryText = 'SELECT * FROM "tasks" ORDER BY "my_task";';
+    const queryText = 'SELECT * FROM "my_tasks" ORDER BY "task";';
     pool.query(queryText).then(result => {
         res.send(result.rows);
     }).catch(error => {
-        console.log('error getting koalas', error);
+        console.log('error getting tasks', error);
         res.sendStatus(500);
     });
 });
@@ -22,8 +22,8 @@ tasksRouter.post('/', (req, res) => {
     const newTask = req.body;
     console.log('adding task', newTask);
     const queryText = `
-                        INSERT INTO "tasks"
-                        ("my_task")
+                        INSERT INTO "my_tasks"
+                        ("task")
                         VALUES
                         ($1);
     `; // Passing req.body values through pg
@@ -32,7 +32,7 @@ tasksRouter.post('/', (req, res) => {
         newTask.my_task,
         //newTask.task_complete
     ]).then((result) => { // sending success back to client
-        console.log('POST new koala success!');
+        console.log('POST new task success!');
         res.sendStatus(200);
     }).catch((error) => {
         console.log('Error in POST', error);
@@ -50,7 +50,7 @@ tasksRouter.post('/', (req, res) => {
 tasksRouter.put('/:id', (req, res) => {
     console.log(req.params);
     const taskId = req.params.id;
-    const queryText = `UPDATE "tasks" 
+    const queryText = `UPDATE "my_tasks" 
                         SET "task_complete" = TRUE 
                         WHERE "id" = $1`;
     pool.query(queryText, [taskId]).then((result) => {
@@ -69,7 +69,7 @@ tasksRouter.put('/:id', (req, res) => {
 tasksRouter.delete('/:id', (req, res) => {
     console.log(req.params);
     const taskId = req.params.id;
-    const queryText = 'DELETE FROM "tasks" WHERE "id"=$1';
+    const queryText = 'DELETE FROM "my_tasks" WHERE "id"=$1';
     pool.query(queryText, [taskId]).then((result) => {
         res.sendStatus(200);
     }).catch((error) => {
