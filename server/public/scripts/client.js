e070db478d3d31a6a4ec9473f0da0005e4ecb5ae
Danny Mussie <dannymussie7@gmail.com>
2021-09-10 19:20:18 -0500

    refereshTasks in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,8 +15,33 @@ function handleSubmit() {
     let task = {};
     task.my_task = $('#task').val();
     addTask(task);
+};
+
+function addTask(taskToAdd) {
+    console.log('in addTask', taskToAdd);
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: taskToAdd,
+    }).then(function(response) {
+        console.log('Response from server.', response);
+        refreshTasks();
+    }).catch(function(error) {
+        console.log('Error in POST', error);
+        alert('Unable to add a task at this time. Please try again later.');
+    });
 }
 
 function refreshTasks() {
     console.log('in refreshTasks');
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function(response) {
+        console.log('GET /books response', response);
+        renderTasks(response);
+    }).catch(function(error) {
+        console.log('error in GET', error);
+    });
 };
+
