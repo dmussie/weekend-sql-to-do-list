32f05bd7c67644578bef1bb5b813befd472678f0
Danny Mussie <dannymussie7@gmail.com>
2021-09-21 12:08:21 -0500

    edited pool.js for heroku deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,14 +1,24 @@
 //require pg which is essential for databases
 const pg = require('pg');
+const url = require('url');
 
-//specify the database details for configuration 
-const config = {
-    database: process.env.DATABASE_NAME || 'weekend-to-do-app', 
-    host: 'localhost', 
-    port: 5432, 
-    max: 10, 
-    idleTimeoutMillis: 30000 
-};
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {rejectUnauthorized: false},
+    };
+} else {
+    config = {
+        database: 'weekend-to-do-app', 
+        host: 'localhost', 
+        port: 5432, 
+        max: 10, 
+        idleTimeoutMillis: 30000 
+    };
+}
+ 
 
 const pool = new pg.Pool(config);
 
@@ -20,6 +30,7 @@ pool.on("connect", () => {
 //indicate connection error
 pool.on("error", (err) => {
     console.log("error connecting to postgres", err);
+    process.exit(-1);
 });
 
 //enables pool data to communicate between server, database, router and client
